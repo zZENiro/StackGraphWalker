e50e07809fe316b8c931ab4d25d943b1e513d3da
Никита <soloistcsgo@yandex.ru>
2020-05-11 01:41:52 +0300

    Factory Method for Graph<T> added

---
+++ Graphs_1/Base.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Graphs_1
+{
+    public abstract class Base
+    {
+        protected bool SetValue<T>(ref T target, T value)
+        {
+            if (object.Equals(target, value))
+                return false;
+
+            target = value;
+            return true;
+        }
+    }
+}
---
+++ Graphs_1/GraphWalker.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Graphs_1
+{
+    public class GraphWalker<T>
+    {
+        enum State
+        {
+            Visited,
+            Unvisted
+        }
+
+        static Dictionary<int, State> visitedNodes = new Dictionary<int, State>();
+
+        public static void WalkGraph<T>(Graph<T> graph)
+        {
+            visitedNodes.Clear();
+            foreach (Node<T> node in graph)
+                visitedNodes.Add(node.Number, State.Unvisted);
+
+            WalkThroughNode(graph.Nodes[0]);
+        }
+
+        static void WalkThroughNode<T>(Node<T> node)
+        {
+            if (node.HasConnections())
+            {
+                visitedNodes[node.Number] = State.Visited;
+                Console.WriteLine(node.Number);
+
+                foreach (var n in node.ConnectedNodes)
+                {
+                    if (visitedNodes[n.Number] == State.Unvisted)
+                        WalkThroughNode(n);
+                }
+            }
+            else
+            {
+                visitedNodes[node.Number] = State.Visited;
+                Console.WriteLine(node.Number);
+
+                return;
+            }
+            return;
+        }
+    }
+}
