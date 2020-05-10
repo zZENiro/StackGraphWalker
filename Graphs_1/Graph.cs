ae9905bb25aaca661bbc1f059575ad2c05a140f8
Никита <soloistcsgo@yandex.ru>
2020-05-11 01:41:52 +0300

    Factory Method for Graph<T> added

--- Graphs_1/Graph.cs
+++ Graphs_1/Graph.cs
@@ -7,16 +7,42 @@ namespace Graphs_1
 {
     public class Graph<T> : IEnumerable
     {
-        private Stack<Node<T>> _nodes;
+        private List<Node<T>> _nodes;
 
-        public Stack<Node<T>> Nodes { get => _nodes; set => _nodes = value; }
+        public List<Node<T>> Nodes { get => _nodes; }
 
-        public Graph()
+        public Graph() => _nodes = new List<Node<T>>();
+
+        public Graph(List<Node<T>> nodes) => _nodes = nodes;
+
+        public static Graph<T> CreateGraph(int[,] graphTable)
         {
-            Nodes = new Stack<Node<T>>();
+            if (!IsQuadratic(graphTable))
+                throw new Exception("Table has to be quad.");
+
+            var crLen = Math.Sqrt(graphTable.Length);
+
+            List<Node<T>> _nodes = new List<Node<T>>();
+            Graph<T> _graph = new Graph<T>(_nodes);
+
+            for (int i = 0; i < graphTable.Length; ++i)
+                _nodes.Add(new Node<T>());
+
+            for (int i = 0; i < crLen; ++i)
+            {
+                for (int j = 0; j < crLen; ++j)
+                {
+                    if (graphTable[i, j] != 0)
+                    {
+                        _nodes[i].ConnectNode(_nodes[j]);
+                    }
+                }
+            }
+
+            return _graph;
         }
 
-        public void AddNode(Node<T> node) => _nodes.Push(node);
+        public void AddNode(Node<T> node) => _nodes.Add(node);
 
         //public void AddNodesRange(params Node<T>[] nodes)
         //{
@@ -27,9 +53,10 @@ namespace Graphs_1
         //public void RemoveNode(Node<T> node)
         //{
         //    node.DisconnectNode();
-
         //}
 
         public IEnumerator GetEnumerator() => ((IEnumerable)_nodes).GetEnumerator();
+        
+        private static bool IsQuadratic(int[,] graphTable) => (Math.Sqrt(graphTable.Length) % 1) == 0;
     }
 }
